22692f42320a1d4f1eb320dae83dd128f2249164
tc <tc@tizilogic.com>
2023-05-02 22:53:57 +0200

    WIP: some initial solve work

--- src/soso/soso.c
+++ src/soso/soso.c
@@ -1,5 +1,6 @@
 #include "soso.h"
-#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define SOSO_NUM_SHUFFLES 12
@@ -10,7 +11,7 @@ static uint64_t random_get_in(uint64_t min, uint64_t max) {
 
 void soso_shuffle(soso_deck_t *deck, uint64_t seed) {
 	soso_random_seed(seed);
-	for (uint8_t i = 0; i < 52; ++i) {
+	for (int8_t i = 0; i < 52; ++i) {
 		deck->cards[i] = i;
 	}
 
@@ -18,7 +19,7 @@ void soso_shuffle(soso_deck_t *deck, uint64_t seed) {
 		for (int i = 0; i < 51; ++i) {
 			int j = random_get_in(i, 51);
 			if (i == j) continue;
-			uint8_t t = deck->cards[j];
+			int8_t t = deck->cards[j];
 			deck->cards[j] = deck->cards[i];
 			deck->cards[i] = t;
 		}
@@ -31,7 +32,7 @@ void soso_deal(soso_game_t *game, soso_deck_t *deck) {
 	for (int i = 0; i < 7; ++i) {
 		game->tableau_top[i] = i + 1;
 		game->tableau_up[i] = i;
-        if (i < 4) game->foundation_top[i] = 0;
+		if (i < 4) game->foundation_top[i] = 0;
 	}
 
 	int next = 0;
@@ -41,13 +42,74 @@ void soso_deal(soso_game_t *game, soso_deck_t *deck) {
 	while (next < 52) game->stock[stock_id--] = deck->cards[next++];
 }
 
-void soso_ctx_init(soso_ctx_t *ctx, int draw_count) {
-    ctx->draw_count = draw_count;
-    ctx->visited = NULL;
-    ctx->moves = NULL;
-    ctx->moves_cap = 0;
-    ctx->moves_top = -1;
-    ctx->moves_total = 0;
+void soso_ctx_init(soso_ctx_t *ctx, int draw_count, void *(*custom_alloc)(size_t),
+                   void *(*custom_realloc)(void *, size_t), void (*custom_free)(void *)) {
+	ctx->draw_count = draw_count;
+	ctx->visited = NULL;
+	ctx->moves = NULL;
+	ctx->moves_cap = 0;
+	ctx->moves_top = 0;
+	ctx->moves_total = 0;
+	if (custom_alloc != NULL) {
+		ctx->alloc = custom_alloc;
+		ctx->realloc = custom_realloc;
+		ctx->free = custom_free;
+	}
+	else {
+		ctx->alloc = malloc;
+		ctx->realloc = realloc;
+		ctx->free = free;
+	}
+}
+
+void soso_ctx_destroy(soso_ctx_t *ctx) {
+	if (ctx->moves != NULL) ctx->free(ctx->moves);
+	if (ctx->visited != NULL) sht_destroy(ctx->visited);
+}
+
+static void grow_moves(soso_ctx_t *ctx) {
+	if (ctx->moves_top >= ctx->moves_cap) {
+		if (ctx->moves_cap == 0) {
+			ctx->moves = (soso_move_t *)ctx->alloc(sizeof(soso_move_t) * 2);
+			ctx->moves_cap = 2;
+		}
+		else {
+			ctx->moves =
+			    (soso_move_t *)ctx->realloc(ctx->moves, sizeof(soso_move_t) * ctx->moves_cap * 2);
+			ctx->moves_cap *= 2;
+		}
+		assert(ctx->moves != NULL);
+	}
+}
+
+static void add_move(soso_ctx_t *ctx, const soso_move_t *m) {
+	grow_moves(ctx);
+	memcpy(&ctx->moves[ctx->moves_top++], m, sizeof(soso_move_t));
+}
+
+static bool move_available(const soso_game_t *game) {}
+
+static void draw(soso_game_t *game) {}
+
+static void make_auto_moves(soso_ctx_t *ctx, soso_game_t *game) {
+	// Turn flipable cards
+	for (int i = 0; i < 7; ++i) {
+		if (game->tableau_top > 0 && game->tableau_top[i] == game->tableau_up[i]) {
+			--game->tableau_up[i];
+			add_move(ctx, &(soso_move_t){.from = i, .to = i, .count = 1, .flip = 1});
+		}
+	}
+
+	// Optionally draw until a move becomes available
+	int snw = game->stock_top + game->waste_top;
+	int numdraw = 0;
+	for (int i = 0; i < snw; ++i) {
+		if (move_available(game)) return;
+		draw(game);
+		++numdraw;
+	}
+	if (numdraw > 0 && numdraw < snw)
+		add_move(ctx, &(soso_move_t){.count = numdraw, .from = -1, .to = -1, .flip = 0});
 }
 
 void soso_solve(soso_ctx_t *ctx, soso_game_t *game) {}
--- src/soso/soso.h
+++ src/soso/soso.h
@@ -2,44 +2,50 @@
 
 #include <sht/sht.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #define SOSO_EMPTY_CARD 255
 
 typedef struct soso_deck {
-    uint8_t cards[52];
+	int8_t cards[52];
 } soso_deck_t;
 
 typedef struct soso_game {
-    uint8_t stock[24];
-    uint8_t waste[24];
-    uint8_t tableau[7][13];
-    uint8_t foundation[4][13];
-    uint8_t stock_top;
-    uint8_t waste_top;
-    uint8_t tableau_top[7];
-    uint8_t tableau_up[7];
-    uint8_t foundation_top[4];
+	int8_t stock[24];
+	int8_t waste[24];
+	int8_t tableau[7][13];
+	int8_t foundation[4][13];
+	int8_t stock_top;
+	int8_t waste_top;
+	int8_t tableau_top[7];
+	int8_t tableau_up[7];
+	int8_t foundation_top[4];
 } soso_game_t;
 
 void soso_shuffle(soso_deck_t *deck, uint64_t seed);
 void soso_deal(soso_game_t *game, soso_deck_t *deck);
 
 typedef struct soso_move {
-    uint8_t from;
-    uint8_t to;
-    uint8_t count;
-    uint8_t flip;
+	int8_t from;
+	int8_t to;
+	int8_t count;
+	int8_t flip;
 } soso_move_t;
 
 typedef struct soso_ctx {
-    int draw_count;
-    sht_t *visited;
-    soso_move_t *moves;
-    int moves_cap, moves_top;
-    int moves_total;
+	int draw_count;
+	sht_t *visited;
+	soso_move_t *moves;
+	int moves_cap, moves_top;
+	int moves_total;
+	void *(*alloc)(size_t);
+	void *(*realloc)(void *, size_t);
+	void (*free)(void *);
 } soso_ctx_t;
 
-void soso_ctx_init(soso_ctx_t *ctx, int draw_count);
+void soso_ctx_init(soso_ctx_t *ctx, int draw_count, void *(*custom_alloc)(size_t),
+                   void *(*custom_realloc)(void *, size_t), void (*custom_free)(void *));
+void soso_ctx_destroy(soso_ctx_t *ctx);
 void soso_solve(soso_ctx_t *ctx, soso_game_t *game);
 
 void soso_random_seed(uint64_t seed);
