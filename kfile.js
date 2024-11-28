a840325fcfa2b2573a15fe70916f8a3660ff8fd2
Tiziano Bettio <tc@tizilogic.com>
2024-11-28 10:55:57 +0100

    Make easier to use as subproject

--- kfile.js
+++ kfile.js
@@ -3,14 +3,18 @@ let project = new Project('soso');
 project.useAsLibrary = () => {
 	project.cmd = false;
 	project.debugDir = null;
+	project.addExclude('tests/test_unit.c');
 };
 
 project.addFile('src/**');
 project.addFile('tests/**');
-project.addFile('ext/**');
-project.addExclude('ext/sht/example.c');
+if (typeof noShtPlease === 'undefined') {
+    project.addFile('ext/sht/sht.c');
+    project.addFile('ext/sht/murmur3.c');
+	project.addIncludeDir('ext');
+    global.noShtPlease = true;
+}
 project.addIncludeDir('src');
-project.addIncludeDir('ext');
 project.setCStd('c99');
 project.setCppStd('c++11');
 project.setDebugDir('bin');
